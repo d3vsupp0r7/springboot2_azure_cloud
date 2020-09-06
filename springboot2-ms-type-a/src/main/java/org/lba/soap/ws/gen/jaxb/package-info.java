815e6dc356c84e69526131c0cd112911d47a063c
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 09:50:57 +0200

    Springboot - Soap WS - Generated classes

---
+++ springboot2-ms-type-a/src/main/java/org/lba/soap/ws/gen/jaxb/package-info.java
@@ -0,0 +1,9 @@
+//
+// Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.7 
+// Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
+// Generato il: 2020.09.05 alle 04:04:58 PM CEST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:8080/ws/spring/soap/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.lba.soap.ws.gen.jaxb;
