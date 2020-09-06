bed727df746a4541c1463c0d446381161922db6c
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 22:44:17 +0200

    Springboot Rest exception management: Employee Not Found

---
+++ springboot2-ms-type-a/src/main/java/org/lba/springboot2/exception/EmployeeNotFoundException.java
@@ -0,0 +1,13 @@
+package org.lba.springboot2.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeNotFoundException(Long id) {
+		super("Could not find employee with id: " + id);
+	}
+}
