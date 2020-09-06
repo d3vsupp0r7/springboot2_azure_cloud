95c5749015425995d1aa7d5d419ff09babb2bf18
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 08:59:15 +0200

    Rest Service layer

---
+++ springboot2-ms-azure-keyvault/src/main/java/org/lba/springboot2/rest/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package org.lba.springboot2.rest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.lba.springboot2.db.model.Employee;
+
+public interface EmployeeService {
+
+	//C
+	public Employee saveEmployee(Employee employee);
+	
+	//R
+	public List<Employee> findAll();
+	
+	public Optional<Employee> findById(Long id);
+	
+	//U
+	public Employee updateEmployee(Long id, Employee employee);
+	
+	//D
+	public void deleteEmployeeById(long id);
+
+}
