de8765841f890c859fd540ccd97cc9e9f8a345f4
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 11:25:59 +0200

    H2 - Initial Load Scripts

---
+++ springboot2-ms-azure-redis-cache/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employee values(10001,'EmployeeName_1', 'EmployeeSurname_1');
+insert into employee values(10002,'EmployeeName_2', 'EmployeeSurname_2');
+insert into employee values(10003,'EmployeeName_3', 'EmployeeSurname_3');
