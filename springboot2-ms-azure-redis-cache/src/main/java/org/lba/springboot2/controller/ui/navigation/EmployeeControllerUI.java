19aeaa36da89e06bf474abd75942748a3af97673
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 11:29:41 +0200

    Springboot controller - Employee scenarios: Navigation and form

---
+++ springboot2-ms-azure-redis-cache/src/main/java/org/lba/springboot2/controller/ui/navigation/EmployeeControllerUI.java
@@ -0,0 +1,17 @@
+package org.lba.springboot2.controller.ui.navigation;
+
+import org.springframework.ui.Model;
+
+public interface EmployeeControllerUI {
+
+	//C
+	
+	//R
+	public String listEmployees(Model model);
+	//U
+	
+	//D
+	
+	//Navigation
+	public String showForm(Model model);
+}
