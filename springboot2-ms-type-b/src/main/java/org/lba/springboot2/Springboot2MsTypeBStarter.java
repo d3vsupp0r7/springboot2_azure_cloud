9cdf4a765240909a209f96834468f81d7e24d19b
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 09:22:11 +0200

    Springboot - Application starters

---
+++ springboot2-ms-type-b/src/main/java/org/lba/springboot2/Springboot2MsTypeBStarter.java
@@ -0,0 +1,19 @@
+package org.lba.springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("org.lba.springboot2.db.repository")
+@ComponentScan({"org.lba.springboot2"})
+@EntityScan("org.lba.springboot2.db.model")
+public class Springboot2MsTypeBStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2MsTypeBStarter.class, args);
+	}
+
+}
