3b2711acce95e6c3bd2e9a46011cdfa37b40b435
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 12:11:11 +0200

    Springboot MS - Azure ServiceBus Producer template

---
+++ springboot2-ms-azure-servicebus-producer/src/test/java/org/lba/springboot2/azure/servicebus/producer/tests/Springboot2MsAzureServiceBusProducerTests.java
@@ -0,0 +1,13 @@
+package org.lba.springboot2.azure.servicebus.producer.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2MsAzureServiceBusProducerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
