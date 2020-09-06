80eed1c08e226a7b8142dcc4e247ac15d001c941
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 11:14:22 +0200

    Springboot Controller - Basic controller for message exchange

---
+++ springboot2-ms-azure-servicebus/src/main/java/org/lba/springboot2/azure/servicebus/AzureServiceBusServiceController.java
@@ -0,0 +1,29 @@
+package org.lba.springboot2.azure.servicebus;
+
+import java.math.BigDecimal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jms.core.JmsTemplate;
+
+@RestController
+public class AzureServiceBusServiceController {
+
+	private static final String DESTINATION_NAME = "<DestinationName>";
+
+    private static final Logger logger = LoggerFactory.getLogger(AzureServiceBusServiceController.class);
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @PostMapping("/messages")
+    public String postMessage(@RequestParam String message) {
+        logger.info("Sending message");
+        jmsTemplate.convertAndSend(DESTINATION_NAME, new MessageData(new BigDecimal("1"), message));
+        return message;
+    }
+}
