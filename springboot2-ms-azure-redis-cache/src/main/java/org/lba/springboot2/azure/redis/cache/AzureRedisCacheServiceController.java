adf7a6ce179fb4230533011cb276abe800d01fd7
d3vsupp0r7 <d3vsupp0r7@example.com>
2020-09-06 11:38:00 +0200

    Springboot -  Controller Template for Azure Redis Cache implementation

---
+++ springboot2-ms-azure-redis-cache/src/main/java/org/lba/springboot2/azure/redis/cache/AzureRedisCacheServiceController.java
@@ -0,0 +1,8 @@
+package org.lba.springboot2.azure.redis.cache;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureRedisCacheServiceController {
+
+}
